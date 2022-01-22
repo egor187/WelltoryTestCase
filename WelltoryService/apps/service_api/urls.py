9ed99251893e2535b778839251c2c607297a3146
egor187 <py.egor.py@gmail.com>
2022-01-22 18:14:52 +0300

    rename urls

--- WelltoryService/apps/service_api/urls.py
+++ WelltoryService/apps/service_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.WeightList.as_view()),
-    path('users/<int:pk>/', views.WeightDetail.as_view()),
+    path('external-fake-api/', views.WeightList.as_view()),
+    path('external-fake-api/<int:pk>/', views.WeightDetail.as_view()),
 ]
