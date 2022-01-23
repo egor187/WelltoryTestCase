c7a5ae27047f7d92700aa1745b5ac5fa95e036a6
egor187 <py.egor.py@gmail.com>
2022-01-23 22:05:17 +0300

    api view for service_importer

--- WelltoryService/apps/service_import/views.py
+++ WelltoryService/apps/service_import/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import reverse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .celery_tasks import import_task
 
-# Create your views here.
+
+SERVICE_API_URL = reverse("service_api")
+
+
+class ImportHandler(APIView):
+    def post(self, request):
+        user_id = request.data.get("user_id")
+        celery_result = import_task(SERVICE_API_URL, user_id)
+        return Response(celery_result)
