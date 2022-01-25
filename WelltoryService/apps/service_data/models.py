d48499abd2ac0839205318ee1263a76df74e26df
egor187 <py.egor.py@gmail.com>
2022-01-25 21:07:32 +0300

    UserWeight for service_data

--- WelltoryService/apps/service_data/models.py
+++ WelltoryService/apps/service_data/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class UserWeight(models.Model):
+    weight = models.FloatField()
+    date = models.DateField(auto_now=True)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="weight")
+
+    def __str__(self):
+        return f"{self.user}: {self.weight}"
