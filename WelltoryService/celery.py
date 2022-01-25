3cd7d876d95795629be334b91d0c5753279185f5
egor187 <py.egor.py@gmail.com>
2022-01-25 20:58:50 +0300

    setup celery backend

--- WelltoryService/celery.py
+++ WelltoryService/celery.py
@@ -3,9 +3,8 @@ import os
 from celery import Celery
 
 
-app = Celery("WelltoryService", broker="redis://redis")
+app = Celery("WelltoryService", broker="redis://redis", backend="redis://redis:6379")
 
 app.config_from_object("django.conf.settings", namespace="CELERY")
 
 app.autodiscover_tasks(related_name="celery_tasks")
-
