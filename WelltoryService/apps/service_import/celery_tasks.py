eba9ae41aff81bdf868b2ac67034c35f70c9d370
egor187 <py.egor.py@gmail.com>
2022-01-25 20:59:28 +0300

    refactor celery task for service_import

--- WelltoryService/apps/service_import/celery_tasks.py
+++ WelltoryService/apps/service_import/celery_tasks.py
@@ -1,13 +1,20 @@
-from celery import shared_task
-from WelltoryService.redis_client import redis_cli
-
 import requests
 
+from WelltoryService.redis_client import redis_cli_prod
+from WelltoryService.celery import app
+from celery.exceptions import SoftTimeLimitExceeded
+from celery.utils.log import get_task_logger
 
-@shared_task
-def import_task(url, user_id):
-    api_url = url + user_id
-    user_data = requests.post(api_url).json()
-    redis_key = f"user_id_redis_key_{str(user_id)}"
-    redis_cli.set(redis_key, user_data)
-    return redis_key
+
+logger = get_task_logger(__name__)
+
+
+@app.task
+def import_task(service_api_url,  redis_key):
+    try:
+        url = f"http://host.docker.internal:8000{service_api_url}"
+        user_data = requests.get(url).text
+        redis_cli_prod.set(redis_key, user_data)
+        return redis_key
+    except SoftTimeLimitExceeded:
+        raise
