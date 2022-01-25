cef2ab90af262f1dfa8600f3db192c6a16e3783c
egor187 <py.egor.py@gmail.com>
2022-01-25 21:01:10 +0300

    setup app_name for reverse api routes

--- WelltoryService/apps/service_api/urls.py
+++ WelltoryService/apps/service_api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'service_api'
+
 urlpatterns = [
     path('', views.WeightList.as_view()),
-    path('<int:pk>/', views.WeightDetail.as_view()),
+    path('<int:pk>/', views.WeightDetail.as_view(), name="detail"),
 ]
