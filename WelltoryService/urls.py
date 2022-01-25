04f8ff5d66e5ccd755ff0cc9f71451a9d0dba672
egor187 <py.egor.py@gmail.com>
2022-01-25 21:01:39 +0300

    update url conf for static serving

--- WelltoryService/urls.py
+++ WelltoryService/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('external-fake-api/', include('WelltoryService.apps.service_api.urls')),
+    path('run-import-task/', include('WelltoryService.apps.service_import.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
