b6de07f6dae1508ad3c6a43b35bb66d64dc0968b
egor187 <py.egor.py@gmail.com>
2022-01-25 21:01:58 +0300

    import_service view

--- WelltoryService/apps/service_import/views.py
+++ WelltoryService/apps/service_import/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
 from django.shortcuts import reverse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .celery_tasks import import_task
+from uuid import uuid4
+from celery.utils.log import get_task_logger
+from celery.exceptions import SoftTimeLimitExceeded
 
-
-SERVICE_API_URL = reverse("service_api")
+logger = get_task_logger(__name__)
 
 
 class ImportHandler(APIView):
     def post(self, request):
+        redis_key = str(uuid4())
         user_id = request.data.get("user_id")
-        celery_result = import_task(SERVICE_API_URL, user_id)
-        return Response(celery_result)
+        service_api_url = reverse("service_api:detail", args=[user_id])
+        try:
+            task_result = import_task.delay(service_api_url, redis_key)
+        except SoftTimeLimitExceeded as e:
+            logger.info(f"Task exc time is exceeded: {e}")
+            return Response(e)
+        else:
+            return Response(task_result.get())
