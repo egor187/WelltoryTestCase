421694fd26a763954fa01c54ee89257c02ed34ef
egor187 <py.egor.py@gmail.com>
2022-01-26 20:21:24 +0300

    service_data views

--- WelltoryService/apps/service_data/views.py
+++ WelltoryService/apps/service_data/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import requests
+from django.urls import reverse
 
-# Create your views here.
+from .serializers import WeightSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .celery_tasks import data_process_task
+from .models import UserWeight
+from WelltoryService.apps.service_import.celery_tasks import import_task
+
+
+class ImportApiView(APIView):
+    serializer_class = WeightSerializer
+
+    def get(self, request):
+        service_import_url = reverse("service_import:import")
+        service_api_url = reverse("service_api:list")
+        url = f"http://host.docker.internal:8000{service_import_url}"
+        redis_key = requests.get(url).text
+        import_task.apply_async(args=[service_api_url, redis_key], link=data_process_task.s())
+        return Response(status=201)
+
+
+class ServiceDataApiView(generics.RetrieveAPIView):
+    queryset = UserWeight.objects.all()
+    serializer_class = WeightSerializer
