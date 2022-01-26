a5ae4659f1dcaee8855b78be875be06304c608a9
egor187 <py.egor.py@gmail.com>
2022-01-26 20:18:46 +0300

    urls for service_data

---
+++ WelltoryService/apps/service_data/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ImportApiView, ServiceDataApiView
+
+
+app_name = 'service_data'
+
+urlpatterns = [
+    path('', ImportApiView.as_view()),
+    path('<int:pk>/', ServiceDataApiView.as_view()),
+]
