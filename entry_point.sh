d794204f7cdf551544b05b67bd21d50bfc76289f
egor187 <py.egor.py@gmail.com>
2022-01-26 21:28:06 +0300

    line sep

--- entry_point.sh
+++ entry_point.sh
@@ -1,1 +1,27 @@
-#!/bin/bash#echo "Flush the manage.py command it any"##while ! python manage.py flush --no-input 2>&1; do#  echo "Flusing django manage command"#  sleep 3#doneecho "Migrate the Database at startup of project"# Wait for few minute and create migrationsecho "Creating migrations"while ! python manage.py makemigrations  2>&1; do   echo "Creating migrations"   sleep 3done# Wait for few minute and run db migraitonwhile ! python manage.py migrate  2>&1; do   echo "Migration is in progress status"   sleep 3doneecho "Django docker is fully configured successfully."exec "$@"
+#!/bin/bash
+
+#echo "Flush the manage.py command it any"
+#
+#while ! python manage.py flush --no-input 2>&1; do
+#  echo "Flusing django manage command"
+#  sleep 3
+#done
+
+echo "Migrate the Database at startup of project"
+
+# Wait for few minute and create migrations
+echo "Creating migrations"
+while ! python manage.py makemigrations  2>&1; do
+   echo "Creating migrations"
+   sleep 3
+done
+
+# Wait for few minute and run db migraiton
+while ! python manage.py migrate  2>&1; do
+   echo "Migration is in progress status"
+   sleep 3
+done
+
+echo "Django docker is fully configured successfully."
+
+exec "$@"
