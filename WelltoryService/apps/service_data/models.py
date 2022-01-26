279cd08b9f1caecd57e5091f2f018fc164b52362
egor187 <py.egor.py@gmail.com>
2022-01-26 20:20:02 +0300

    untied UserWeight model from django Users model

--- WelltoryService/apps/service_data/models.py
+++ WelltoryService/apps/service_data/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
 
 class UserWeight(models.Model):
     weight = models.FloatField()
-    date = models.DateField(auto_now=True)
-    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="weight")
+    day = models.DateField(auto_now=True)
+    user_id = models.IntegerField()
 
     def __str__(self):
-        return f"{self.user}: {self.weight}"
+        return f"{self.user_id}: {self.weight}"
